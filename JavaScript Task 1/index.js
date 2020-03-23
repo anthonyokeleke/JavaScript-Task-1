332bc3c8a9183ce2115f4c5f32422146a9957482
Anthony Okeleke <61886182+anthonyokeleke@users.noreply.github.com>
2020-03-23 21:02:17 +0100

    Add files via upload

---
+++ JavaScript Task 1/index.js
@@ -0,0 +1,11 @@
+let name = 'Anthony Okeleke';
+console.log("My name is: " + name);
+
+let courses = ['Python', 'HTML/CSS/JS', 'UI/UX', 'Kotlin']
+console.log("Courses: " + courses);
+
+for (let i = 0; i<= 200; i++) {
+    if (i%2!== 1) {
+        console.log(i + " ");
+    }
+}
